68f2b985f2098c5d069ddd15a0e739fb05dd9806
Vedant Thorat <167152687+Vedantthorat@users.noreply.github.com>
2026-01-25 09:05:46 +0530

    Add insertion sort implementation for sorting colors
    Implement insertion sort algorithm to sort colors in-place.

---
+++ insrtion_sort_75.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # insertion sort
+       
+        for i in range(1,len(nums)):
+            j = i - 1
+            key = nums[i]
+            while j >= 0 and  nums[j] > key:
+                nums[j+1] = nums[j]
+                j -=1
+            nums[j+1] = key
+        
+     
+
