5f2a8701f115a9881a7f181dc4b641f8cbe1b2bb
Vedant Thorat <167152687+Vedantthorat@users.noreply.github.com>
2026-02-06 12:33:02 +0530

    Create Single_Number_136.py

---
+++ Single_Number_136.py
@@ -0,0 +1,36 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
+
+
+'''
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+'''
