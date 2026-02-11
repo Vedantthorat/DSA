83cbac442808d110d4887feacb4e9cff310a56b1
Vedant Thorat <167152687+Vedantthorat@users.noreply.github.com>
2026-02-11 10:31:22 +0530

    Create Merge_sort.py

---
+++ Merge_sort.py
@@ -0,0 +1,30 @@
+def merge_sort(arr):
+    if len (arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    res = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            res.append(left[i])
+            i += 1
+
+        else:
+            res.append(right[j])
+
+            j += 1
+
+    res.extend(left[i:])
+    res.extend(right[j:])
+
+    return res
+
+
+arr = [34,43,54,33,77,22,99]
+print(merge_sort(arr))
