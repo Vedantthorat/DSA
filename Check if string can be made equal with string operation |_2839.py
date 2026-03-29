448e395ce52e910b2a333b9f7f211d17541b4617
Vedant Thorat <167152687+Vedantthorat@users.noreply.github.com>
2026-03-29 07:48:34 +0530

    Add method to check string equality via operations
    Implement a method to check if two strings can be made equal by sorting pairs of characters.

---
+++ Check if string can be made equal with string operation |_2839.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        
+        return sorted([s1[0], s1[2]]) == sorted([s2[0],s2[2]]) and \
+               sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]])
+
+  '''
+Example 1:
+
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: We can do the following operations on s1:
+- Choose the indices i = 0, j = 2. The resulting string is s1 = "cbad".
+- Choose the indices i = 1, j = 3. The resulting string is s1 = "cdab" = s2.
+Example 2:
+
+Input: s1 = "abcd", s2 = "dacb"
+Output: false
+Explanation: It is not possible to make the two strings equal.
+
+  '''
